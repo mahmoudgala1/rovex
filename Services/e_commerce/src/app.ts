8a439f25c8248f42f6507c604aa29008b28a1444
Mahmoud Galal <mahmoudgalal173.95@gmail.com>
2025-12-18 18:53:20 +0200

    Add docker compose file to handle nginx and other backend services

--- Services/e_commerce/src/app.ts
+++ Services/e_commerce/src/app.ts
@@ -1,22 +1,27 @@
-import express,{Request, Response, Application, NextFunction} from 'express';
-import { LoggerMiddleware } from './middlewares/logger.middleware';
+import express, { Request, Response, Application, NextFunction } from "express";
+import { LoggerMiddleware } from "./middlewares/logger.middleware";
 import productRoutes from "./routers/products.routers";
 import cartRoutes from "./routers/cart.routers";
-import { validateRequiredFields } from './middlewares/globalValidation.middleware';
-import { AppError } from './utils/AppError';
-import { globalErrorHandeler } from './middlewares/globalerror.middleware';
+import { validateRequiredFields } from "./middlewares/globalValidation.middleware";
+import { AppError } from "./utils/AppError";
+import { globalErrorHandeler } from "./middlewares/globalerror.middleware";
 
+const app: Application = express();
+app.use(express.json());
+app.use(LoggerMiddleware);
 
-const app:Application = express();
-app.use(express.json())
-app.use(LoggerMiddleware)
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+    service: "rovex-e_commerce-service",
+  });
+});
 
-app.use('/api/v1/products',productRoutes);    
-app.use('/api/v1/cart',cartRoutes);    
-app.use('/api/v1/coupon',cartRoutes);    
+app.use("/api/v1/products", productRoutes);
+app.use("/api/v1/cart", cartRoutes);
+app.use("/api/v1/coupon", cartRoutes);
 
-
-
-
-app.use(globalErrorHandeler)
+app.use(globalErrorHandeler);
 export default app;
