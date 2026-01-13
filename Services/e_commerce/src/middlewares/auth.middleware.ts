fd1dbd9d70dd08bcbe3259346f0adfa679065df5
hassanAlzyatt <hassanalzyatcse2022@gmail.com>
2026-01-13 10:15:38 -0500

    replace mock data with dynamic auth middleware data

--- Services/e_commerce/src/middlewares/auth.middleware.ts
+++ Services/e_commerce/src/middlewares/auth.middleware.ts
@@ -7,16 +7,17 @@ import { Types } from 'mongoose';
 export const extractUserFromHeaders = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     
     // 1. Extract user info from custom headers set by Nginx
-    // const user_id = req.headers['x-user-id'] as string;
-    // const user_role = req.headers['x-user-role'] as string;
-    // const user_type = req.headers['x-user-type'] as string;
-    // const company_id = req.headers['x-company-id'] as string
+    const user_id = new Types.ObjectId(req.headers['x-user-id'] as string);
+     const user_role = new Types.ObjectId(req.headers['x-user-role'] as string);
+     const user_type = req.headers['x-user-type'] as string
+     const company_id = new Types.ObjectId(req.headers['x-company-id'] as string) || null;
     //mokup data
-     const user_id = new Types.ObjectId("64b5f8c12345678901230000");
-    const user_role = "customer";
-    const user_type = "";
+
+    //const user_id = new Types.ObjectId("64b5f8c12345678901230000");
+    // const user_role = "customer";
+    // const user_type = "";
     // const company_id = new Types.ObjectId("64b5f8c12345678901234567");
-    const company_id = null
+   // const company_id = null
 
     // 2. Validate: If Nginx is configured correctly, these should NEVER be missing.
     if (!user_id || !user_role) {
